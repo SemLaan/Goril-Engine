0cedd299bf2e687593e02976b40274a68d142605
semlaan <semlaan@dds.nl>
2023-08-11 01:48:54 +0200

    Changed the memory subsystem tests

--- Tests/src/core/memory_tests.cpp
+++ Tests/src/core/memory_tests.cpp
@@ -7,18 +7,18 @@
 
 b8 alloc_and_dealloc_test()
 {
-	size_t initial_allocated = GR::GetMemoryUsage();
+	u64 initial_allocation_count = GR::GetNetAllocations();
 	size_t test_allocation_size = 18;
 
-	GR::Blk test_mem = GR::Alloc(test_allocation_size, GR::memory_tag::MEM_TAG_LINEAR_ALLOC);
+	GR::Blk test_mem = GR::GetGlobalAllocator()->Alloc(test_allocation_size, GR::mem_tag::TEST);
 
 	expect_to_be_true(test_mem.ptr != 0);
-	expect_should_be(test_mem.length, test_allocation_size);
-	expect_should_be(GR::GetMemoryUsage() - initial_allocated, test_allocation_size);
+	expect_should_be(test_mem.size, test_allocation_size);
+	expect_should_be(GR::GetNetAllocations() - initial_allocation_count, 1);
 
-	GR::Free(test_mem);
+	GR::GetGlobalAllocator()->Free(test_mem);
 
-	expect_should_be(GR::GetMemoryUsage(), initial_allocated);
+	expect_should_be(GR::GetNetAllocations(), initial_allocation_count);
 
 	return true;
 }
