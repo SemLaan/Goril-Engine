1110e30d4444a71c799c781b449e5b01e672c9c4
semlaan <semlaan@dds.nl>
2023-08-13 00:30:55 +0200

    WindowProc function now handles one message differently than the default

--- Goril/src/platform/platform_win32.cpp
+++ Goril/src/platform/platform_win32.cpp
@@ -93,7 +93,14 @@ namespace GR
 
 	LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
-		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+		switch (uMsg)
+		{
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			return 0;
+		default:
+			return DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}
 	}
 }
 #endif
