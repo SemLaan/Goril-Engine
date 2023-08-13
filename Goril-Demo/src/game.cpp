bf7cb7fec0526f9bed70b8f0eec38b6ccd2dfb75
semlaan <semlaan@dds.nl>
2023-08-13 20:55:32 +0200

    Removed some event testing from the game code

--- Goril-Demo/src/game.cpp
+++ Goril-Demo/src/game.cpp
@@ -1,38 +1,18 @@
 #include "game.h"
 #include <core/gr_memory.h>
-#include <core/event.h>
 #include <core/logger.h>
 
 using namespace GR;
 
-b8 Beef(EventType type, EventData data)
-{
-    GRDEBUG("Test, {}, {}, {}, {}", data.u32[0], data.u32[1], data.u32[2], data.u32[3]);
-    return false;
-}
 
 b8 Game::Init()
 {
     PrintMemoryStats();
-    RegisterEventListener(EVCODE_QUIT, Beef);
-    EventData data;
-    data.u32[0] = 3;
-    data.u32[1] = 4;
-    data.u32[2] = 5;
-    data.u32[3] = 6;
-    InvokeEvent(EVCODE_QUIT, data);
-    UnregisterEventListener(EVCODE_QUIT, Beef);
     return true;
 }
 
 b8 Game::Update()
 {
-    EventData data;
-    data.u32[0] = 3;
-    data.u32[1] = 4;
-    data.u32[2] = 5;
-    data.u32[3] = 6;
-    InvokeEvent(EVCODE_QUIT, data);
     return true;
 }
 
