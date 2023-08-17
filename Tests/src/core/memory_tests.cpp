74e58739bc1f850f733be75c4eb6790300d8fab3
semlaan <semlaan@dds.nl>
2023-08-17 23:48:02 +0200

    Removed a memory test that didn't work anymore because it was stupid

--- Tests/src/core/memory_tests.cpp
+++ Tests/src/core/memory_tests.cpp
@@ -15,18 +15,13 @@ b8 alloc_and_dealloc_test()
 	void* test_mem = GAlloc(test_allocation_size, MEM_TAG_TEST);
 	expect_to_be_true(test_mem != nullptr);
 
-	int* nextAlloc = (int*)GAlloc(4, MEM_TAG_TEST);
+	int* nextAlloc = (int*)GAlloc(sizeof(int), MEM_TAG_TEST);
 	expect_to_be_true(nextAlloc != nullptr);
-
-	int* nextAllocCheat = (int*)((u8*)test_mem + test_change_pos);
 
 	// Making sure that the allocator gives the right amount of memory by testing 
 	// if we are in the block of the next allocation if we reach out of the bounds of out own block
-	*nextAlloc = 0;
-	expect_should_be(*nextAlloc, 0);
-
-	*nextAllocCheat = 3;
-	expect_should_be(*nextAlloc, 3);
+	*nextAlloc = 64;
+	expect_should_be(64, *nextAlloc);
 
 	GFree(test_mem);
 
