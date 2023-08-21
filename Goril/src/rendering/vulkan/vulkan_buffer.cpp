911b64790147fd7680594f9e8b572b0ea5c376b6
semlaan <semlaan@dds.nl>
2023-08-21 02:27:46 +0200

    Made a staging buffer and a final buffer, though data doesn't get copied between them yet

--- Goril/src/rendering/vulkan/vulkan_buffer.cpp
+++ Goril/src/rendering/vulkan/vulkan_buffer.cpp
@@ -31,37 +31,71 @@ namespace GR
 		buffer->vertices.Pushback({ {0.5f, 0.5f}, {0.f, 1.f, 0.f} });
 		buffer->vertices.Pushback({ {-0.5f, 0.5f}, {0.f, 0.f, 1.f} });
 
+		// ================ Staging buffer =========================
+		VkBuffer stagingBuffer;
+		VkDeviceMemory stagingMemory;
+
+		VkBufferCreateInfo stagingBufferCreateInfo = {};
+		stagingBufferCreateInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+		stagingBufferCreateInfo.pNext = nullptr;
+		stagingBufferCreateInfo.flags = 0;
+		stagingBufferCreateInfo.size = sizeof(Vertex) * buffer->vertices.Size();
+		stagingBufferCreateInfo.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
+		stagingBufferCreateInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+		stagingBufferCreateInfo.queueFamilyIndexCount = 0;
+		stagingBufferCreateInfo.pQueueFamilyIndices = nullptr;
+
+		if (VK_SUCCESS != vkCreateBuffer(vk_state->device, &stagingBufferCreateInfo, vk_state->allocator, &stagingBuffer))
+			GRASSERT(false);
+
+		VkMemoryRequirements stagingMemoryRequirements;
+		vkGetBufferMemoryRequirements(vk_state->device, stagingBuffer, &stagingMemoryRequirements);
+		
+		VkMemoryAllocateInfo stagingAllocateInfo = {};
+		stagingAllocateInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+		stagingAllocateInfo.pNext = nullptr;
+		stagingAllocateInfo.allocationSize = stagingMemoryRequirements.size;
+		stagingAllocateInfo.memoryTypeIndex = FindMemoryType(vk_state->physicalDevice, stagingMemoryRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+
+		if (VK_SUCCESS != vkAllocateMemory(vk_state->device, &stagingAllocateInfo, vk_state->allocator, &stagingMemory))
+			GRASSERT(false);
+
+		vkBindBufferMemory(vk_state->device, stagingBuffer, stagingMemory, 0);
+
+		// ================= copying data into staging buffer ===============================
+		void* data;
+		vkMapMemory(vk_state->device, stagingMemory, 0, stagingBufferCreateInfo.size, 0, &data);
+		MemCopy(data, buffer->vertices.GetRawElements(), (size_t)stagingBufferCreateInfo.size);
+		vkUnmapMemory(vk_state->device, stagingMemory);
+
+		// ================= creating the actual buffer =========================
 		VkBufferCreateInfo bufferCreateInfo = {};
 		bufferCreateInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
 		bufferCreateInfo.pNext = nullptr;
 		bufferCreateInfo.flags = 0;
 		bufferCreateInfo.size = sizeof(Vertex) * buffer->vertices.Size();
-		bufferCreateInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
-		bufferCreateInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
-		//bufferCreateInfo.queueFamilyIndexCount = 1;
-		//bufferCreateInfo.pQueueFamilyIndices = &state->queueIndices.graphicsFamily;
+		bufferCreateInfo.usage = VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+		bufferCreateInfo.sharingMode = VK_SHARING_MODE_CONCURRENT; /// TODO: use memory barriers instead of concurrent
+		bufferCreateInfo.queueFamilyIndexCount = 2;
+		u32 queueFamilyIndices[2] = { vk_state->queueIndices.graphicsFamily, vk_state->queueIndices.transferFamily };
+		bufferCreateInfo.pQueueFamilyIndices = queueFamilyIndices;
 
 		if (VK_SUCCESS != vkCreateBuffer(vk_state->device, &bufferCreateInfo, vk_state->allocator, &buffer->handle))
 			GRASSERT(false);
 
 		VkMemoryRequirements memoryRequirements;
 		vkGetBufferMemoryRequirements(vk_state->device, buffer->handle, &memoryRequirements);
-		
+
 		VkMemoryAllocateInfo allocateInfo = {};
 		allocateInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
 		allocateInfo.pNext = nullptr;
 		allocateInfo.allocationSize = memoryRequirements.size;
-		allocateInfo.memoryTypeIndex = FindMemoryType(vk_state->physicalDevice, memoryRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+		allocateInfo.memoryTypeIndex = FindMemoryType(vk_state->physicalDevice, memoryRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
 
 		if (VK_SUCCESS != vkAllocateMemory(vk_state->device, &allocateInfo, vk_state->allocator, &buffer->memory))
 			GRASSERT(false);
 
 		vkBindBufferMemory(vk_state->device, buffer->handle, buffer->memory, 0);
-
-		void* data;
-		vkMapMemory(vk_state->device, buffer->memory, 0, bufferCreateInfo.size, 0, &data);
-		MemCopy(data, buffer->vertices.GetRawElements(), (size_t)bufferCreateInfo.size);
-		vkUnmapMemory(vk_state->device, buffer->memory);
 
 		return clientBuffer;
 	}
