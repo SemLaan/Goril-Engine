370cc96a48217423e80c48d12e4315277ba3d8c0
semlaan <semlaan@dds.nl>
2023-09-23 16:05:28 +0200

    Added debugBreak() for gcc and fixed an assert printing but

--- Goril/src/core/asserts.h
+++ Goril/src/core/asserts.h
@@ -1,14 +1,17 @@
 #pragma once
 #include "logger.h"
 
-//#include <intrin.h>
+#ifdef __GNUC__
+#define debugBreak() __builtin_trap()
+#else
 #define debugBreak() __debugbreak()
+#endif
 
-#define GRASSERT(expr) {if (expr){} else { GRFATAL("Assertion fail: %s, File: %s, Line: %s", #expr, __FILE__, __LINE__); debugBreak();}}
-#define GRASSERT_MSG(expr, message) {if (expr){} else { GRFATAL("Assertion fail: %s, Message: %s, File: %s, Line: %s", #expr, message, __FILE__, __LINE__); debugBreak();}}
+#define GRASSERT(expr) {if (expr){} else { GRFATAL("Assertion fail: %s, File: %s, Line: %i", #expr, __FILE__, __LINE__); debugBreak();}}
+#define GRASSERT_MSG(expr, message) {if (expr){} else { GRFATAL("Assertion fail: %s, Message: %s, File: %s, Line: %i", #expr, message, __FILE__, __LINE__); debugBreak();}}
 
 #ifdef GR_DEBUG
-#define GRASSERT_DEBUG(expr) {if (expr){} else { GRFATAL("Assertion fail: %s, File: %s, Line: %s", #expr, __FILE__, __LINE__); debugBreak();}}
+#define GRASSERT_DEBUG(expr) {if (expr){} else { GRFATAL("Assertion fail: %s, File: %s, Line: %i", #expr, __FILE__, __LINE__); debugBreak();}}
 #else
 #define GRASSERT_DEBUG(expr)
 #endif
