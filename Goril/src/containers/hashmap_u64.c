004be2a9a510601cfe3d42b9d7a4f9c60098ee3e
semlaan <semlaan@dds.nl>
2023-10-06 17:14:44 +0200

    Added a hashmap that uses u64's as keys

---
+++ Goril/src/containers/hashmap_u64.c
@@ -0,0 +1,116 @@
+#include "hashmap_u64.h"
+
+#include "core/asserts.h"
+#include <string.h>
+
+
+HashmapU64* MapU64Create(Allocator* allocator, mem_tag memtag, u32 backingArrayElementCount, u32 maxCollisions, HashFunctionU64 hashFunction)
+{
+    HashmapU64* hashmap = Alloc(allocator, sizeof(*hashmap) + backingArrayElementCount * sizeof(MapEntryU64), memtag);
+    hashmap->backingArray = (MapEntryU64*)(hashmap + 1);
+    hashmap->backingArrayElementCount = backingArrayElementCount;
+    hashmap->hashFunction = hashFunction;
+    hashmap->linkedEntryPool = CreatePoolAllocator(sizeof(MapEntryU64), maxCollisions);
+    hashmap->allocator = allocator;
+
+    memset(hashmap->backingArray, 0, sizeof(MapEntryU64) * backingArrayElementCount);
+
+    return hashmap;
+}
+
+void MapU64Destroy(HashmapU64* hashmap)
+{
+    DestroyPoolAllocator(hashmap->linkedEntryPool);
+
+    Free(hashmap->allocator, hashmap);
+}
+
+void MapU64Insert(HashmapU64* hashmap, u64 key, void* value)
+{
+    u32 hash = hashmap->hashFunction(key) % hashmap->backingArrayElementCount;
+
+    // TODO: when in debug build check if the entry is already in the map
+
+    MapEntryU64* currentEntry = &hashmap->backingArray[hash];
+
+    while (nullptr != currentEntry->value)
+    {
+        if (currentEntry->next != nullptr)
+        {
+            currentEntry = currentEntry->next;
+        }
+        else
+        {
+            currentEntry->next = Alloc(&hashmap->linkedEntryPool, sizeof(MapEntryU64), MEM_TAG_HASHMAP);
+            memset(currentEntry->next, 0, sizeof(MapEntryU64));
+            currentEntry = currentEntry->next;
+        }
+    }
+
+    currentEntry->key = key;
+    currentEntry->value = value;
+}
+
+void* MapU64Lookup(HashmapU64* hashmap, u64 key)
+{
+    u32 hash = hashmap->hashFunction(key) % hashmap->backingArrayElementCount;
+
+    MapEntryU64* currentEntry = &hashmap->backingArray[hash];
+
+    while (true)
+    {
+        if (currentEntry->key == key)
+        {
+            return currentEntry->value;
+        }
+        else if (currentEntry->next == nullptr)
+        {
+            return nullptr;
+        }
+        else
+        {
+            currentEntry = currentEntry->next;
+        }
+    }
+}
+
+void* MapU64Delete(HashmapU64* hashmap, u64 key)
+{
+    u32 hash = hashmap->hashFunction(key) % hashmap->backingArrayElementCount;
+
+    MapEntryU64* currentEntry = &hashmap->backingArray[hash];
+    MapEntryU64* previousEntry = nullptr;
+
+    while (true)
+    {
+        if (currentEntry->key == key)
+        {
+            u64 returnValue = currentEntry->value;
+            // If there's no previous entry, meaning that current entry is in the backing array
+            if (previousEntry == nullptr && currentEntry->next != nullptr)
+            {
+                MemCopy(currentEntry, currentEntry->next, sizeof(*currentEntry));
+            }
+            else if (previousEntry == nullptr) // First entry and there is no next entry (next is nullptr)
+            {
+                memset(currentEntry, 0, sizeof(*currentEntry));
+            }
+            else // if there is a previous entry, meaning current entry is in a linked list and NOT in the backing array
+            {
+                previousEntry->next = currentEntry->next;
+                Free(&hashmap->linkedEntryPool, currentEntry);
+            }
+            return returnValue;
+        }
+        else if (currentEntry->next == nullptr)
+        {
+            GRWARN("HashmapU64: Tried to delete item that doesn't exist, key: %llu", key);
+            return nullptr;
+        }
+        else
+        {
+            currentEntry = currentEntry->next;
+        }
+    }
+}
+
---
+++ Goril/src/containers/hashmap_u64.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "defines.h"
+#include "core/gr_memory.h"
+
+
+typedef u32 (*HashFunctionU64)(u64 key);
+
+
+typedef struct MapEntryU64
+{
+    u64 key;
+    void* value;
+    struct MapEntryU64* next;
+} MapEntryU64;
+
+typedef struct HashmapU64
+{
+    HashFunctionU64 hashFunction;
+    Allocator linkedEntryPool;
+    Allocator* allocator;
+    MapEntryU64* backingArray;
+    u32 backingArrayElementCount;
+} HashmapU64;
+
+// Creates a map, objects are to be kept track of outside of the hashmap
+// note that the hashmap stores pointers to objects, so those pointers can not be invalidated
+// or the hashmap has outdated pointers
+// backingArrayElementCount: amount of elements in the backing array
+HashmapU64* MapU64Create(Allocator* allocator, mem_tag memtag, u32 backingArrayElementCount, u32 maxCollisions, HashFunctionU64 hashFunction);
+
+// Destroys everything about the map, except the objects
+void MapU64Destroy(HashmapU64* hashmap);
+
+// Inserts item into map, asserts if the key is already in the map
+void MapU64Insert(HashmapU64* hashmap, u64 key, void* value);
+
+// Returns a void pointer to the found object or nullptr if the object wasn't found
+void* MapU64Lookup(HashmapU64* hashmap, u64 key);
+
+// Returns the deleted element
+void* MapU64Delete(HashmapU64* hashmap, u64 key);
