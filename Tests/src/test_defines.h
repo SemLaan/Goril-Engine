9620716b46e4b521c6f5dfea35719a770c268e55
semlaan <semlaan@dds.nl>
2023-10-07 01:32:05 +0200

    Fixed the test_defines printf formatting bug

--- Tests/src/test_defines.h
+++ Tests/src/test_defines.h
@@ -13,32 +13,33 @@
 
 #define expect_should_be(expected, actual) \
     if ((actual) != (expected)) {                                                                           \
-        GRERROR("--> Expected {}, but got: {}. File: %s:%s.", expected, actual, __FILE__, __LINE__); \
+        GRERROR("--> Expected %llu, but got: %llu. File: %s:%u.", expected, actual, __FILE__, __LINE__); \
         return false;                                                                                   \
     }
 
 #define expect_should_not_be(expected, actual) \
     if ((actual) == (expected)) {                                                                                    \
-        GRERROR("--> Expected {} != {}, but they are equal. File: %s:%s.", expected, actual, __FILE__, __LINE__); \
+        GRERROR("--> Expected %llu != %llu, but they are equal. File: %s:%u.", expected, actual, __FILE__, __LINE__); \
         return false;                                                                                            \
     }
 
 #define expect_float_to_be(expected, actual) \
     if (abs((expected) - (actual)) > 0.001f) {                                                         \
-        GRERROR("--> Expected %f, but got: %f. File: %s:%s.", expected, actual, __FILE__, __LINE__); \
+        GRERROR("--> Expected %f, but got: %f. File: %s:%u.", expected, actual, __FILE__, __LINE__); \
         return false;                                                                               \
     }
 
 #define expect_to_be_true(actual) \
     if ((actual) != true) {                                                              \
-        GRERROR("--> Expected true, but got: false. File: %s:%s.", __FILE__, __LINE__); \
+        GRERROR("--> Expected true, but got: false. File: %s:%u.", __FILE__, __LINE__); \
         return false;                                                                  \
     }
 
 #define expect_to_be_false(actual) \
     if ((actual) != false) {                                                             \
-        GRERROR("--> Expected false, but got: true. File: %s:%s.", __FILE__, __LINE__); \
+        GRERROR("--> Expected false, but got: true. File: %s:%u.", __FILE__, __LINE__); \
         return false;                                                                  \
     }
+
 
     
