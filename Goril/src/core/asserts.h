a63e2f4d73f7cd9e2ffd6714a439beb7e0884e1d
semlaan <semlaan@dds.nl>
2023-10-08 18:38:20 +0200

    Fixed something in the assert printing

--- Goril/src/core/asserts.h
+++ Goril/src/core/asserts.h
@@ -7,11 +7,11 @@
 #define debugBreak() __debugbreak()
 #endif
 
-#define GRASSERT(expr) {if (expr){} else { GRFATAL("Assertion fail: %s, File: %s, Line: %i", #expr, __FILE__, __LINE__); debugBreak();}}
-#define GRASSERT_MSG(expr, message) {if (expr){} else { GRFATAL("Assertion fail: %s, Message: %s, File: %s, Line: %i", #expr, message, __FILE__, __LINE__); debugBreak();}}
+#define GRASSERT(expr) {if (expr){} else { GRFATAL("Assertion fail: %s, File: %s:%i", #expr, __FILE__, __LINE__); debugBreak();}}
+#define GRASSERT_MSG(expr, message) {if (expr){} else { GRFATAL("Assertion fail: %s, Message: %s, File: %s:%i", #expr, message, __FILE__, __LINE__); debugBreak();}}
 
 #ifdef GR_DEBUG
-#define GRASSERT_DEBUG(expr) {if (expr){} else { GRFATAL("Assertion fail: %s, File: %s, Line: %i", #expr, __FILE__, __LINE__); debugBreak();}}
+#define GRASSERT_DEBUG(expr) {if (expr){} else { GRFATAL("Assertion fail: %s, File: %s:%i", #expr, __FILE__, __LINE__); debugBreak();}}
 #else
 #define GRASSERT_DEBUG(expr)
 #endif
