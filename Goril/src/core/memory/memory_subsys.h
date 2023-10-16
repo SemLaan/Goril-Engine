9c71c70e13ac6257f0d4390e2a62ebb438146f39
semlaan <semlaan@dds.nl>
2023-10-16 22:53:41 +0200

    Removed more commented out code

--- Goril/src/core/memory/memory_subsys.h
+++ Goril/src/core/memory/memory_subsys.h
@@ -14,20 +14,3 @@ bool InitializeMemory(size_t requiredMemory, size_t subsysMemoryRequirement);
 void ShutdownMemory();
 
 Allocator* GetGlobalAllocator();
-
-/*
-const u64 GetMemoryUsage();
-
-const u64 GetNetAllocations();
-
-void PrintMemoryStats();
-
-
-#ifndef GR_DIST // These functions only get compiled if it's not a distribution build
-void AllocInfo(size_t size, MemTag tag);
-
-void ReAllocInfo(i64 sizeChange);
-
-void FreeInfo(size_t size, MemTag tag);
-#endif // !GR_DIST
-*/
